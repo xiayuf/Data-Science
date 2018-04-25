39e99d51fafc7562c672084e7f5ec58af2df88d5
Xia YuFeng <tiandaxyf@gmail.com>
2018-04-25 11:11:45 +0800

    Update lec01.py

--- lec01.py
+++ lec01.py
@@ -10,12 +10,16 @@ from urllib.request import urlopen
 import re
 def read_url(url): 
     return re.sub('\\s+', ' ', urlopen(url).read().decode())
+
 little_women_url = 'https://www.inferentialthinking.com/chapters/01/3/little_women.txt'
 little_women_text = read_url(little_women_url)
 chapters = little_women_text.split('CHAPTER ')[1:]
 #Table().with_column('Text', chapters)
+
 np.char.count(chapters, 'Christmas')
+
 np.char.count(chapters, 'Jo')
+
 Table().with_columns(
     'Jo', np.char.count(chapters, 'Jo'),
     'Meg', np.char.count(chapters, 'Meg'),
